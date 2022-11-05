4a3ec622a8f1f50bbe027eed2787495116c52f0c
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-05 19:46:35 +0600

    environment setup

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import ServiceCard from './ServiceCard';
 
 const Services = () => {
 
@@ -14,13 +15,16 @@ const Services = () => {
         <div>
             <div className='text-center'>
                 <p className='text-2xl font-bold text-orange-600'>Services</p>
-                <h2 className='text-5xl font-semibold '>Our Service Area</h2>
-                <p>There Are Many Variations Of Passages Of Available, But The Majority Have Suffered Alternation In Some Form</p>
+                <h2 className='text-5xl mt-4 mb-4 font-semibold '>Our Service Area</h2>
+                <p className='mb-8'>There Are Many Variations Of Passages Of Available, But The Majority Have Suffered Alternation In Some Form</p>
             </div>
-            <div>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                
                 {
-
+                    services.map(service => <ServiceCard
+                    key={service._id}
+                    service={service}
+                    ></ServiceCard>)
                 }
             </div>
         </div>
