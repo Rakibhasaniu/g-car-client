6adb632273ee4d1dc5cfce379fd95c71f5262a2f
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2022-11-05 13:39:50 +0600

    header and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
 import './App.css'
 import {  RouterProvider } from 'react-router-dom'
 import router from './Router/Routes/Routes'
@@ -8,7 +8,7 @@ function App() {
  
 
   return (
-    <div className="App">
+    <div >
       <RouterProvider router={router}></RouterProvider>
     </div>
   )
